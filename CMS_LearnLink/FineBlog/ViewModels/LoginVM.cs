b361d29acdbe2e8959c85392d79e7c493cf74d41
bigboi00 <leearthhur02@gmail.com>
2024-07-26 21:24:51 +0800

    Finalized CMS Version

---
+++ CMS_LearnLink/FineBlog/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace FineBlog.ViewModels
+{
+    public class LoginVM
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Username is required.")]
+        public string? Username { get; set; }
+        [DataType(DataType.Password)]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
